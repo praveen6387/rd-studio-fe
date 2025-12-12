34c132fd96f74f6e1c5c4f6e6b820ad43cb77117
Praveen Maurya <praveenmaurya@Praveens-MacBook-Air.local>
2025-12-13 00:37:58 +0530

    flipbook ui changes

--- src/components/utils/mediaViewModal/_builder/FlipbookView.jsx
+++ src/components/utils/mediaViewModal/_builder/FlipbookView.jsx
@@ -1,49 +1,64 @@
 "use client";
-import { RotateCcwIcon } from "lucide-react";
-import React, { useState } from "react";
+import React from "react";
 import HTMLFlipBook from "react-pageflip";
 
 const FlipbookView = ({ media_title, media_description, media_library_items }) => {
   const pages = media_library_items.map((item) => ({ url: item.media_url, title: item.media_item_title }));
-  const [isRotated, setIsRotated] = useState(false);
+  let new_pages = [];
+  for (let i = 0; i < pages.length; i++) {
+    if (i === 0 || i === pages.length - 1) {
+      // First page stays full
+      new_pages.push({ url: pages[i].url, title: pages[i].title, cropSide: "full" });
+    } else {
+      // Subsequent pages: split into left and right halves
+      new_pages.push({ url: pages[i].url, title: pages[i].title, cropSide: "left" });
+      new_pages.push({ url: pages[i].url, title: pages[i].title, cropSide: "right" });
+    }
+  }
 
   return (
     <>
-      <button
-        className="absolute top-2 right-12 z-50 bg-black/50 p-2 rounded-full"
-        onClick={() => setIsRotated(!isRotated)}
-      >
-        <RotateCcwIcon className="w-4 h-4 text-white" />
-      </button>
-      <div
-        className={`flex flex-col items-center justify-center w-full h-full
-        transition-transform duration-300 ${isRotated ? "rotate-90" : ""} p-16`}
-      >
+      <div className={`w-screen h-screen flex items-center justify-center`}>
         <HTMLFlipBook
-          key={isRotated ? "rotated" : "normal"}
-          width={isRotated ? 300 : 600}
-          height={isRotated ? 200 : 400}
-          maxShadowOpacity={1.0}
+          width={600}
+          height={400}
           showCover={true}
           mobileScrollSupport={true}
-          usePortrait={!isRotated}
+          usePortrait={true}
           startPage={0}
-          drawShadow={true}
           useMouseEvents={true}
-          swipeDistance={80}
-          showPageCorners={true}
-          autoSize={true}
-          // hard="cover"
+          // mode="landscape"
+          flipFrom="middle"
         >
-          {pages.map((page, idx) => (
+          {new_pages.map((page, idx) => (
             <div key={idx} data-density="hard">
-              <img
-                src={page.url}
-                alt={page.title}
-                // className="max-w-full max-h-full object-contain"
-                style={{ width: "100%", height: "100%" }}
-              />
-              {/* <div className="text-center text-xs text-gray-500 mt-2">{page.title}</div> */}
+              {page.cropSide == "full" && <img src={page.url} alt={page.title} className={`w-full h-full`} />}
+              {page.cropSide == "left" && (
+                <div
+                  className={`w-full h-full`}
+                  style={{
+                    backgroundImage: `url(${page.url})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "left top",
+                    backgroundSize: "200% 100%",
+                  }}
+                  aria-label={page.title}
+                  role="img"
+                />
+              )}
+              {page.cropSide == "right" && (
+                <div
+                  className={`w-full h-full`}
+                  style={{
+                    backgroundImage: `url(${page.url})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "right top",
+                    backgroundSize: "200% 100%",
+                  }}
+                  aria-label={page.title}
+                  role="img"
+                />
+              )}
             </div>
           ))}
         </HTMLFlipBook>
